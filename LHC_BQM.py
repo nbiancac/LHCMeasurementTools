b3ad7cb2b1d3faf1160627f15eddf93d9e073cc5
giadarol <giovanni.iadarola@cern.ch>
2015-07-06 16:34:07 +0200

    introduced consistency check

--- LHC_BQM.py
+++ LHC_BQM.py
@@ -4,52 +4,53 @@ import TimberManager as tm
 class filled_buckets:
     def __init__(self, timber_variable, beam=0):
 
-        if type(timber_variable) is str:
-            if not (beam == 1 or beam == 2):
+        if not (beam == 1 or beam == 2):
                 raise ValueError('You need to specify which beam! (1 or 2)')
+
+        if type(timber_variable) is str:
             dict_timber = tm.parse_timber_file(timber_variable, verbose=True)
             timber_variable_filled = dict_timber[get_variable_dict(beam)['FILLED_BUCKETS']]
         elif type(timber_variable) is dict:
-            timber_variable_filled = timber_variable[get_variable_dict(beam)['FILLED_BUCKETS']]            
+            timber_variable_filled = timber_variable[get_variable_dict(beam)['FILLED_BUCKETS']]
         self.t_stamps = np.array(timber_variable_filled.t_stamps)
-        self.fillbuck = timber_variable_filled.values		
+        self.fillbuck = timber_variable_filled.values
         self.fillbuck = map(lambda x: np.array(map(lambda y: int(float(y)), x)), self.fillbuck)
         self.fillbuck = map(lambda x: (x-1)/10, self.fillbuck)
         self.fillbuck = map(lambda x: x[x>=0], self.fillbuck)
 
         self.Nbun = map(len, self.fillbuck)
         N_acq = len(self.Nbun)
-            
+
         self.flag_filled = []
         for ii in xrange(N_acq):
             self.flag_filled.append(np.array(map(lambda x: x in self.fillbuck[ii], range(3564))))
-			
-		
+
+
     def nearest_older_sample(self, t_obs):
         ind_min = np.argmin(np.abs(self.t_stamps - t_obs))
         if self.t_stamps[ind_min] > t_obs:
             ind_min -= 1
-		
+
         if ind_min == -1:
             return 0.*self.fillbuck[ind_min]
-        else:	
+        else:
             return self.fillbuck[ind_min]
-			
+
     def nearest_older_sample_flag_filled_Nbun(self, t_obs):
         ind_min = np.argmin(np.abs(self.t_stamps - t_obs))
         if self.t_stamps[ind_min] > t_obs:
-            ind_min -= 1 
+            ind_min -= 1
 
         if ind_min == -1:
             return [], 0
-        else:	
+        else:
             return self.flag_filled[ind_min], self.Nbun[ind_min]
 
 
-		
+
 class blength:
     def __init__(self,timber_variable_blength, timber_variable_filled_bucket=None, beam=0):
-        
+
         if type(timber_variable_blength) is str:
             if not (beam == 1 or beam == 2):
                 raise ValueError('You need to specify which beam! (1 or 2)')
@@ -74,23 +75,23 @@ class blength:
             timber_variable_filled_bucket = dict_timber[get_variable_dict(beam)['FILLED_BUCKETS']]
             fillbuck_obj = filled_buckets(timber_variable_filled_bucket)
         elif isinstance(timber_variable_filled_bucket, filled_buckets):
-            fillbuck_obj = timber_variable_filled_bucket 
+            fillbuck_obj = timber_variable_filled_bucket
         elif timber_variable_filled_bucket == None:
             raise TypeError('You need to provide a filled_bucket file or object!')
-		
+
         self.t_stamps = timber_variable_blength.t_stamps
         self.blen = []
         blen_timberstyle = timber_variable_blength.values
         blen_timberstyle = map(lambda x: np.array(map(float, x)), blen_timberstyle)
-	
+
         N_acq = len(self.t_stamps)
-        
+
         for ii in xrange(N_acq):
-            blen_vect = np.zeros(3564) 
+            blen_vect = np.zeros(3564)
             flag_filled_curr, Nbun_curr = fillbuck_obj.nearest_older_sample_flag_filled_Nbun(self.t_stamps[ii])
             blen_vect[flag_filled_curr] = (blen_timberstyle[ii][:Nbun_curr])
             self.blen.append(blen_vect)
-			
+
         self.t_stamps = np.array(self.t_stamps)
         self.avblen = np.array(map(mean_nonzero, self.blen))
 
@@ -99,16 +100,16 @@ class blength:
         ind_min = np.argmin(np.abs(self.t_stamps - t_obs))
         if self.t_stamps[ind_min]>t_obs:
             ind_min -= 1
-	
-        if flag_return_time:	
+
+        if flag_return_time:
             if ind_min == -1:
                 return 0.*self.blen[ind_min], -1
-            else:	
+            else:
                 return self.blen[ind_min], self.t_stamps[ind_min]
         else:
             if ind_min == -1:
                 return 0.*self.blen[ind_min]
-            else:	
+            else:
                 return self.blen[ind_min]
 
 
@@ -116,7 +117,7 @@ def mean_nonzero(x):
     mask_nonzero = x > 0
     if np.sum(mask_nonzero) > 0:
         return np.mean(x[mask_nonzero])
-    else:		
+    else:
         return 0.
 
 
@@ -131,6 +132,6 @@ def get_variable_dict(beam):
 def variable_list(beams = [1,2]):
     var_list = []
     for beam in beams:
-		var_list += get_variable_dict(beam).values()
+                var_list += get_variable_dict(beam).values()
 
     return var_list
