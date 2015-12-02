052a68754555a2b43b0cb0e0f5b05c1ba79e17ef
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2015-12-02 15:15:18 +0200

    minor compatibility fix

--- LHC_Energy.py
+++ LHC_Energy.py
@@ -12,7 +12,7 @@ class energy:
             timber_variable_energy = timber_variable[get_variable_dict(beam)['ENERGY']]
 
         self.t_stamps = timber_variable_energy.t_stamps
-        self.energy = np.atleast_1d(np.squeeze(timber_variable_energy.values))
+        self.energy = np.atleast_1d(np.squeeze(timber_variable_energy.float_values()))
         #self.energy = map(lambda x: float(x[0]), self.energy)
 	
         self.t_stamps = np.array(self.t_stamps)
